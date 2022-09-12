c8ba259ea0b14d51cde6dc7c045fef300331fea6
Lawson <lawsonmcphetridge2020@gmail.com>
2022-09-12 15:58:36 -0700

    sorry i forgot to acp rendered animal on page

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,13 @@
-export default function Animal() {
-  return <></>;
+import './Animal.css';
+
+
+export default function Animal({ animal }) {
+  return <>
+    <div className="animal" style={{ top: animal.top, left: animal.left }} >
+      <p className="name">{animal.name}</p>
+      {animal.type}
+      {animal.says}
+      <img src={`${process.env.PUBLIC_URL}/animals/${animal.type}.svg`} />
+    </div>
+  </>;
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import { animals } from '../../data';
 import './Main.css';
+import Animal from '../Animal/Animal';
+import background from '../../background.png';
+
+
 
 export default function Main() {
-  return <main></main>;
+  return <main style={{ backgroundImage: `url(${background})` }}>
+    {animals.map((animal) => (
+      <Animal key={animal.name} animal={animal} />   
+    ))}
+
+    
+
+
+
+
+  </main>;
 }
